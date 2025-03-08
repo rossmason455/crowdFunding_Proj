4ebfade28c126924e47c8c20b0e8c8f92890f55d
Ross Mason <N00230645@iadt.ie>
2025-03-08 11:24:20 +0000

    established rel between campaigns and user

--- app/Models/Campaigns.php
+++ app/Models/Campaigns.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Campaigns extends Model
 {
-    //
+    // The Campaigns model represents the 'animals' table in the database and defines the fillable attributes for mass assignment.
+
+    protected $fillable = [
+        'title',
+        'description',
+        'progress',
+        'solution',
+        'start_date',
+        'end_date',
+        'competitive_landscape',
+        'team',
+        'use_of_funds',
+        'user_id',
+    ];
+
+
+
+    public function user(){
+        return $this->hasMany(User::class);
+    }
+
+
+
+
 }
+
