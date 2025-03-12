9342f34393e0f44ee684fd316ad963a08268205b
Ross Mason <N00230645@iadt.ie>
2025-03-12 19:49:58 +0000

    added dashboard controller and edited home.show.blade to display campaign details

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.app')
 
+@section('content')
+
 <h3 class="font-weight-semibold h3 mb-4">List of Campaigns:</h3>
 <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-2 g-4">
     @foreach($campaigns as $campaign)
         <div class="col">
+
+        <a href="{{ route('home.show', $campaign) }}">
             <x-campaign-card :title="$campaign->title"
                              :description="$campaign->description"
                              :goal="$campaign->goal"
                              :campaign_type="$campaign->campaign_type"
                              :image="$campaign->campaignImages->first()->image ?? ''"/>
+                             </a>
         </div>
     @endforeach
 </div>
-
+@endsection
                         
                         
                         
---
+++ resources/views/home/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+
+        <x-slot name="header">
+            <h2 class="fw-semibold fs-4 text-dark">
+                {{ $campaign->title }} Details
+            </h2>
+        </x-slot>
+
+        <div class="py-5"
+            style="background-image: url('{{ asset('images/animal-background.jpg') }}'); background-size: cover; height: 60vh;">
+            <div class="container">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <x-campaign-details :image="$campaign->campaignImages->first()->image ?? ''"
+                            :title="$campaign->title" :description="$campaign->description"
+                            :solution="$campaign->solution" :start_date="$campaign->start_date"
+                            :end_date="$campaign->end_date" :goal="$campaign->goal" :progress="$campaign->progress"
+                            :competitive_landscape="$campaign->competitive_landscape" :team="$campaign->team"
+                            :use_of_funds="$campaign->use_of_funds" :campaign_type="$campaign->campaign_type" />
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+</div>
+@endsection
