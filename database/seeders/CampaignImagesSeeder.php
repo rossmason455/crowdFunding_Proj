ecc31ede7034a6a1505fc00548a6ad5940907fa1
Ross Mason <N00230645@iadt.ie>
2025-03-12 14:50:33 +0000

    changed placeholder images in campaign images and reseeded the table

--- database/seeders/CampaignImagesSeeder.php
+++ database/seeders/CampaignImagesSeeder.php
@@ -15,70 +15,395 @@ class CampaignImagesSeeder extends Seeder
     {
         CampaignImage::insert([
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/banner/AGTC.jpg',
                 'campaign_id' => 1
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/AGTC.jpg',
+                'campaign_id' => 1
+              
+            ],
+            [
+                'image' => '/images/solution/AGTC.jpg',
+                'campaign_id' => 1
+              
+            ],
+            [
+                'image' => '/images/team/AGTC.jpg',
+                'campaign_id' => 1
+              
+            ],
+            [
+                'image' => '/images/banner/Ava.jpg',
                 'campaign_id' => 2
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Ava.jpg',
+                'campaign_id' => 2
+              
+            ],
+            [
+                'image' => '/images/solution/Ava.jpg',
+                'campaign_id' => 2
+              
+            ],
+            [
+                'image' => '/images/team/Ava.jpg',
+                'campaign_id' => 2
+              
+            ],
+            [
+                'image' => '/images/banner/Brains.jpg',
+                'campaign_id' => 3
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/Brains.jpg',
+                'campaign_id' => 3
+              
+            ],
+            [
+                'image' => '/images/solution/Brains.jpg',
+                'campaign_id' => 3
+              
+            ],
+            [
+                'image' => '/images/team/Brains.jpg',
+                'campaign_id' => 3
+              
+            ],
+            [
+                'image' => '/images/banner/Cintap.jpg',
                 'campaign_id' => 4
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Cintap.jpg',
+                'campaign_id' => 4
+              
+            ],
+            [
+                'image' => '/images/solution/Cintap.jpg',
+                'campaign_id' => 4
+              
+            ],
+            [
+                'image' => '/images/team/Cintap.jpg',
+                'campaign_id' => 4
+              
+            ],
+            [
+                'image' => '/images/banner/DB.jpg',
                 'campaign_id' => 5
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/DB.jpg',
+                'campaign_id' => 5
+              
+            ],
+            [
+                'image' => '/images/solution/DB.jpg',
+                'campaign_id' => 5
+              
+            ],
+            [
+                'image' => '/images/team/DB.jpg',
+                'campaign_id' => 5
+              
+            ],
+            [
+                'image' => '/images/banner/EZ.jpg',
                 'campaign_id' => 6
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/EZ.jpg',
+                'campaign_id' => 6
+              
+            ],
+            [
+                'image' => '/images/solution/EZ.jpg',
+                'campaign_id' => 6
+              
+            ],
+            [
+                'image' => '/images/team/EZ.jpg',
+                'campaign_id' => 6
+              
+            ],
+            [
+                'image' => '/images/banner/Fivvy.jpg',
+                'campaign_id' => 7
+            ],
+            [
+                'image' => '/images/competitive_landscape/Fivvy.jpg',
                 'campaign_id' => 7
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/solution/Fivvy.jpg',
+                'campaign_id' => 7
+              
+            ],
+            [
+                'image' => '/images/team/Fivvy.jpg',
+                'campaign_id' => 7
+              
+            ],
+            [
+                'image' => '/images/banner/InventorPrism.jpg',
+                'campaign_id' => 8
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/InventorPrism.jpg',
+                'campaign_id' => 8
+              
+            ],
+            [
+                'image' => '/images/solution/InventorPrism.jpg',
+                'campaign_id' => 8
+              
+            ],
+            [
+                'image' => '/images/team/InventorPrism.jpg',
+                'campaign_id' => 8
+              
+            ],
+            [
+                'image' => '/images/banner/LuxuryVacationClub.jpg',
                 'campaign_id' => 9
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/LuxuryVacationClub.jpg',
+                'campaign_id' => 9
+              
+            ],
+            [
+                'image' => '/images/solution/LuxuryVacationClub.jpg',
+                'campaign_id' => 9
+              
+            ],
+            [
+                'image' => '/images/team/LuxuryVacationClub.jpg',
+                'campaign_id' => 9
+              
+            ],
+            [
+                'image' => '/images/banner/Msolar.jpg',
                 'campaign_id' => 10
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Msolar.jpg',
+                'campaign_id' => 10
+              
+            ],
+            [
+                'image' => '/images/solution/Msolar.jpg',
+                'campaign_id' => 10
+              
+            ],
+            [
+                'image' => '/images/team/Msolar.jpg',
+                'campaign_id' => 10
+              
+            ],
+            [
+                'image' => '/images/banner/Neighbr.jpg',
                 'campaign_id' => 11
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Neighbr.jpg',
+                'campaign_id' => 11              
+            ],
+            [
+                'image' => '/images/solution/Neighbr.jpg',
+                'campaign_id' => 11
+              
+            ],
+            [
+                'image' => '/images/team/Neighbr.jpg',
+                'campaign_id' => 11
+              
+            ],
+            [
+                'image' => '/images/banner/Pixels.jpg',
                 'campaign_id' => 12
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Pixels.jpg',
+                'campaign_id' => 12             
+            ],
+            [
+                'image' => '/images/solution/Pixels.jpg',
+                'campaign_id' => 12
+              
+            ],
+            [
+                'image' => '/images/team/Pixels.jpg',
+                'campaign_id' => 12
+              
+            ],
+            [
+                'image' => '/images/banner/Replastic.jpg',
+                'campaign_id' => 13
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/Replastic.jpg',
+                'campaign_id' => 13              
+            ],
+            [
+                'image' => '/images/solution/Replastic.jpg',
+                'campaign_id' => 13
+              
+            ],
+            [
+                'image' => '/images/team/Replastic.jpg',
+                'campaign_id' => 13
+              
+            ],
+            [
+                'image' => '/images/banner/Rizz.jpg',
                 'campaign_id' => 14
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Rizz.jpg',
+                'campaign_id' => 14              
+            ],
+            [
+                'image' => '/images/solution/Rizz.jpg',
+                'campaign_id' => 14
+              
+            ],
+            [
+                'image' => '/images/team/Rizz.jpg',
+                'campaign_id' => 14
+              
+            ],
+            [
+                'image' => '/images/banner/Sigma.jpg',
                 'campaign_id' => 15
               
             ],
             [
-                'image' => '/images/perks/Perks_image.jpg',
+                'image' => '/images/competitive_landscape/Sigma.jpg',
+                'campaign_id' => 15              
+            ],
+            [
+                'image' => '/images/solution/Sigma.jpg',
+                'campaign_id' => 15
+              
+            ],
+            [
+                'image' => '/images/team/Sigma.jpg',
+                'campaign_id' => 15
+              
+            ],
+            [
+                'image' => '/images/banner/Syntra.jpg',
                 'campaign_id' => 16
               
             ],
+            [
+                'image' => '/images/competitive_landscape/Syntra.jpg',
+                'campaign_id' => 16              
+            ],
+            [
+                'image' => '/images/solution/Syntra.jpg',
+                'campaign_id' => 16
+              
+            ],
+            [
+                'image' => '/images/team/Syntra.jpg',
+                'campaign_id' => 16
+              
+            ],
+            [
+                'image' => '/images/banner/Tiba.jpg',
+                'campaign_id' => 17
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/Tiba.jpg',
+                'campaign_id' => 17              
+            ],
+            [
+                'image' => '/images/solution/Tiba.jpg',
+                'campaign_id' => 17
+              
+            ],
+            [
+                'image' => '/images/team/Tiba.jpg',
+                'campaign_id' => 17
+              
+            ],
+            [
+                'image' => '/images/banner/UnknownCyber.jpg',
+                'campaign_id' => 18
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/UnknownCyber.jpg',
+                'campaign_id' => 18              
+            ],
+            [
+                'image' => '/images/solution/UnknownCyber.jpg',
+                'campaign_id' => 18
+              
+            ],
+            [
+                'image' => '/images/team/UnknownCyber.jpg',
+                'campaign_id' => 18
+              
+            ],
+            [
+                'image' => '/images/banner/Vega.jpg',
+                'campaign_id' => 19
+              
+            ],
+            [
+                'image' => '/images/competitive_landscape/Vega.jpg',
+                'campaign_id' => 19              
+            ],
+            [
+                'image' => '/images/solution/Vega.jpg',
+                'campaign_id' => 19
+              
+            ],
+            [
+                'image' => '/images/team/Vega.jpg',
+                'campaign_id' => 19
+              
+            ], 
+            [
+                'image' => '/images/banner/BakeryProcessSolutions.jpg',
+                'campaign_id' => 20
+            ],
+            [
+                'image' => '/images/competitive_landscape/BakeryProcessSolutions.jpg',
+                'campaign_id' => 20              
+            ],
+            [
+                'image' => '/images/solution/BakeryProcessSolutions.jpg',
+                'campaign_id' => 20
+              
+            ],
+            [
+                'image' => '/images/team/BakeryProcessSolutions.jpg',
+                'campaign_id' => 20
+              
+            ],       
+
+
 
             
         ]);
