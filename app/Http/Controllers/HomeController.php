ea688f7e41ea1654ee224b93ccc413c599e3e351
Ross Mason <N00230645@iadt.ie>
2025-03-18 19:31:36 +0000

    added logic to profileSearch for filtering profiles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,10 +20,30 @@ class HomeController extends Controller
 
 
 
-    public function profileSearch()
+    public function profileSearch(Request $request)
     {
   
-        $profiles = InvestorProfile::with('user')->get();
+        $query = InvestorProfile::with('user');
+
+        if ($request->filled('risk_profile')) {
+            $query->where('risk_profile', $request->risk_profile);
+        }
+    
+        if ($request->filled('min_investment')) {
+            $query->where('min_investment', '>=', $request->min_investment);
+        }
+    
+        if ($request->filled('max_investment')) {
+            $query->where('max_investment', '<=', $request->max_investment);
+        }
+    
+        if ($request->filled('investment_preference')) {
+            $query->where('investment_preference', 'like', '%' . $request->investment_preference . '%');
+        }
+    
+
+        $profiles = $query->paginate(10);
+
 
        
         return view('home.profileSearch', compact('profiles'));
