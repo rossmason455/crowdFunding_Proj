5c5db4f131e7fb82a95d4ee63ef60f8c4ffe56d1
Ross Mason <N00230645@iadt.ie>
2025-03-26 00:14:55 +0000

    added dump and die for debugging error passing campaign id to the database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -400,6 +400,8 @@ class HomeController extends Controller
             'currency' => 'usd',
         ]);
 
+        dd($campaign);
+
         $transaction = Transaction::create([
             'amount' => $request->amount,
             'stripe_transaction_id' => $paymentIntent->id,
@@ -410,7 +412,7 @@ class HomeController extends Controller
             'user_id' => auth()->id(),
         ]);
 
-        return redirect()->route('home.index')->with('success', 'Perk updated successfully!');
+        return redirect()->route('home.index', )->with('success', 'Perk updated successfully!');
     }
 
 //
