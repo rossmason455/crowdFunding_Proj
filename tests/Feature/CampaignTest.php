1412ed0ba1b151044a3c2ad328cb025e8ba869a6
Ross Mason <N00230645@iadt.ie>
2025-03-28 15:03:38 +0000

    added functions for testing te

--- tests/Feature/CampaignTest.php
+++ tests/Feature/CampaignTest.php
@@ -83,57 +83,209 @@ class CampaignTest extends TestCase
 
 
 
-        public function test_read_campaign(){
+//        public function test_read_campaign(){
+//
+//            $user = User::create([
+//                
+//                'name' => 'John Melrose',
+//                'email' => 'john123@examplee.com',
+//                'email_verified_at' => now(),
+//                'password' => bcrypt('hello'), 
+//                'remember_token' => '84QI5BKtiA',
+//                'role' => 'campaigner',
+//                'image' => 'image.jpg',
+//                'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
+//                'pm_type' => 'amex',
+//                'pm_last_four' => '1234',
+//                'trial_ends_at' => null
+//            ]);
+//
+//
+//
+//
+//    $validated  = [
+//        'title' => 'New Campaign',
+//        'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+//         'progress' => 10,
+//
+//        'goal' => 10000.00,
+//        'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+//        'start_date' => '2025-01-01',  
+//        'end_date' => '2025-12-31',
+//      
+//        'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+//        'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+//        'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+//         
+//        'campaign_type' => 'Crowdfunding',
+//        
+//        'user_id' => $user->id,  
+//    ];
+//
+//    $campaign = Campaign::create($validated);
+//
+//
+//    $response = $this->actingAs($user)
+//             ->get(route('home.show', $campaign));
+//
+//
+//    $response->assertStatus(200);
+//    $response->assertSee($campaign->title);
+//    $response->assertSee($campaign->description); 
+//
+//        }
 
-            $user = User::create([
+
+
+
+
+    
+//public function test_update_campaign(){
+//    
+//    $user = User::create([
+//                
+//        'name' => 'John Melrose',
+//        'email' => 'john123@examplee.com',
+//        'email_verified_at' => now(),
+//        'password' => bcrypt('hello'), 
+//        'remember_token' => '84QI5BKtiA',
+//        'role' => 'campaigner',
+//        'image' => 'image.jpg',
+//        'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
+//        'pm_type' => 'amex',
+//        'pm_last_four' => '1234',
+//        'trial_ends_at' => null
+//    ]);
+//
+//
+//
+//
+//$validated  = [
+//'title' => 'New Campaign',
+//'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+// 'progress' => 10,
+//
+//'goal' => 10000.00,
+//'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+//'start_date' => '2025-01-01',  
+//'end_date' => '2025-12-31',
+//
+//'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+//'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+//'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+// 
+//'campaign_type' => 'Crowdfunding',
+//
+//'user_id' => $user->id,  
+//];
+//
+//$campaign = Campaign::create($validated);
+//
+//$updatedData = [
+//    'title' => 'Updated Campaign',
+//    'description' => 'Updated description.',
+//    'progress' => 50,
+//    'goal' => 20000.00,
+//    'solution' => 'Updated solution text.',
+//    'start_date' => '2025-01-01',
+//    'end_date' => '2025-12-31',
+//    'competitive_landscape' => 'Updated landscape text.',
+//    'team' => 'Updated team text.',
+//    'use_of_funds' => 'Updated use of funds.',
+//    'campaign_type' => 'Angel Investment', 
+//    'user_id' => $user->id
+//];
+//
+//
+//$response = $this->actingAs($user)
+//->put(route('home.updateCampaign', $campaign), $updatedData);
+//
+//
+//
+//$response->assertStatus(302); 
+//
+//
+//$campaign->refresh();
+//
+//
+//$this->assertEquals($updatedData['title'], $campaign->title);
+//$this->assertEquals($updatedData['description'], $campaign->description);
+//$this->assertEquals($updatedData['goal'], $campaign->goal);
+//$this->assertEquals($updatedData['solution'], $campaign->solution);
+//$this->assertEquals($updatedData['campaign_type'], $campaign->campaign_type);
+//
+//}
+
+public function test_delete_campaign(){
+    
+    $user = User::create([
                 
-                'name' => 'John Melrose',
-                'email' => 'john123@examplee.com',
-                'email_verified_at' => now(),
-                'password' => bcrypt('hello'), 
-                'remember_token' => '84QI5BKtiA',
-                'role' => 'campaigner',
-                'image' => 'image.jpg',
-                'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
-                'pm_type' => 'amex',
-                'pm_last_four' => '1234',
-                'trial_ends_at' => null
-            ]);
+        'name' => 'John Melrose',
+        'email' => 'john123@examplee.com',
+        'email_verified_at' => now(),
+        'password' => bcrypt('hello'), 
+        'remember_token' => '84QI5BKtiA',
+        'role' => 'campaigner',
+        'image' => 'image.jpg',
+        'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
+        'pm_type' => 'amex',
+        'pm_last_four' => '1234',
+        'trial_ends_at' => null
+    ]);
 
 
 
 
-    $validated  = [
-        'title' => 'New Campaign',
-        'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-         'progress' => 10,
+$validated  = [
+'title' => 'New Campaign',
+'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+ 'progress' => 10,
 
-        'goal' => 10000.00,
-        'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        'start_date' => '2025-01-01',  
-        'end_date' => '2025-12-31',
-      
-        'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-        'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-        'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
-         
-        'campaign_type' => 'Crowdfunding',
-        
-        'user_id' => $user->id,  
-    ];
+'goal' => 10000.00,
+'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+'start_date' => '2025-01-01',  
+'end_date' => '2025-12-31',
 
-    $campaign = Campaign::create($validated);
+'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+ 
+'campaign_type' => 'Crowdfunding',
+
+'user_id' => $user->id,  
+];
+
+$campaign = Campaign::create($validated);
+
+$updatedData = [
+    'title' => 'Updated Campaign',
+    'description' => 'Updated description.',
+    'progress' => 50,
+    'goal' => 20000.00,
+    'solution' => 'Updated solution text.',
+    'start_date' => '2025-01-01',
+    'end_date' => '2025-12-31',
+    'competitive_landscape' => 'Updated landscape text.',
+    'team' => 'Updated team text.',
+    'use_of_funds' => 'Updated use of funds.',
+    'campaign_type' => 'Angel Investment', 
+    'user_id' => $user->id
+];
 
 
-    $response = $this->actingAs($user)
-             ->get(route('home.show', $campaign));
+$response = $this->actingAs($user)
+->delete(route('home.destroyCampaign', $campaign->id));
 
 
-    $response->assertStatus(200);
-    $response->assertSee($campaign->title);
-    $response->assertSee($campaign->description); 
 
-        }
+$response->assertStatus(302); 
+
+
+
+
+}
+
+
 
 
 }
