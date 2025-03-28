17f8a0cbce160f5240a012178d18c6e7b054dcf0
Ross Mason <N00230645@iadt.ie>
2025-03-28 13:57:03 +0000

    added function for testing read of campaign

--- tests/Feature/CampaignTest.php
+++ tests/Feature/CampaignTest.php
@@ -13,59 +13,129 @@ class CampaignTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_create_campaign(): void
-    {   
+   // public function test_create_campaign(): void
+   // {   
+//
+   //    $user = User::firstOrCreate([
+   //                 'id' => 20,
+   //             ], [
+   //                'name' => 'John Melrose',
+   //         'email' => 'john123@examplee.com',
+   //         'email_verified_at' => now(),
+   //         'password' => 'hello',
+   //         'remember_token' => '84QI5BKtiA',
+   //         'role' => 'campaigner',
+   //         'image' => 'image.jpg',
+   //         'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
+   //         'pm_type' => 'amex',
+   //         'pm_last_four' => '1234',
+   //         'trial_ends_at' => null
+   //             ]);
+//
+//
+//
+//
+   //     $validated  = [
+   //         'title' => 'New Campaign',
+   //         'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+   //          'progress' => 10,
+//
+   //         'goal' => 10000.00,
+   //         'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+   //         'start_date' => '2025-01-01',  
+   //         'end_date' => '2025-12-31',
+   //       
+   //         'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+   //         'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+   //         'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+   //          
+   //         'campaign_type' => 'Crowdfunding',
+   //         
+   //         'user_id' => $user->id,  
+   //     ];
+//
+   //     $campaign = Campaign::create($validated);
+//
+   //     $response = $this->post(route('home.storeCampaign'));
+//
+   //     $this->assertDatabaseHas('campaigns', [
+   //         'title' => 'New Campaign',
+   //         'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+   //          'progress' => 10,
+//
+   //         'goal' => 10000.00,
+   //         'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+   //        'start_date' => '2025-01-01',  
+   //         'end_date' => '2025-12-31',
+   //       
+   //         'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+   //         'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+   //         'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+   //          'campaign_type' => 'Crowdfunding',
+   //         'user_id' => $user->id,  
+   //     ]);
+//
+//
+   //     $response->assertRedirect(route('home.index'));
+   // 
+   // }
 
-        $user = User::find(20);
-        if (!$user) {
-            throw new \Exception('User with ID 20 does not exist');
+
+
+
+        public function test_read_campaign(){
+
+            $user = User::create([
+                
+                'name' => 'John Melrose',
+                'email' => 'john123@examplee.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('hello'), 
+                'remember_token' => '84QI5BKtiA',
+                'role' => 'campaigner',
+                'image' => 'image.jpg',
+                'stripe_id' => '6c1deb66-478c-4046-9fe1-6cc80f9831f0',
+                'pm_type' => 'amex',
+                'pm_last_four' => '1234',
+                'trial_ends_at' => null
+            ]);
+
+
+
+
+    $validated  = [
+        'title' => 'New Campaign',
+        'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+         'progress' => 10,
+
+        'goal' => 10000.00,
+        'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'start_date' => '2025-01-01',  
+        'end_date' => '2025-12-31',
+      
+        'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+        'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
+        'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
+         
+        'campaign_type' => 'Crowdfunding',
+        
+        'user_id' => $user->id,  
+    ];
+
+    $campaign = Campaign::create($validated);
+
+
+    $response = $this->actingAs($user)
+             ->get(route('home.show', $campaign));
+
+
+    $response->assertStatus(200);
+    $response->assertSee($campaign->title);
+    $response->assertSee($campaign->description); 
+
         }
 
 
-
-
-        $validated  = [
-            'title' => 'New Campaign',
-            'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-             'progress' => 10,
-
-            'goal' => 10000,
-            'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-            'start_date' => now()->subMonths(4),
-            'end_date' => now()->addMonths(8),
-          
-            'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-            'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-            'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
-             'campaign_type' => 'Crowdfunding',
-             'user_id' => null
-        ];
-
-        $campaign = Campaign::create($validated);
-
-        $response = $this->post(route('home.storeCampaign'));
-
-        $this->assertDatabaseHas('campaigns', [
-            'title' => 'New Campaign',
-            'description' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-             'progress' => 10,
-
-            'goal' => 10000,
-            'solution' => 'standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-            
-            
-            'start_date' => now()->subMonths(4),
-            'end_date' => now()->addMonths(8),
-            'competitive_landscape' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-            'team' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset',
-            'use_of_funds' => 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus Page',
-             'campaign_type' => 'Crowdfunding',
-             'user_id' => null
-        ]);
-
-
-        $response->assertRedirect(route('home.dashboard'));
-    }
 }
 
 
