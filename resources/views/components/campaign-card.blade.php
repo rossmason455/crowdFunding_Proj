e29d491a848c02e2eac3ba0314c08e361ae5e34a
Ross Mason <N00230645@iadt.ie>
2025-04-01 13:28:27 +0100

    made all cards the same height

--- resources/views/components/campaign-card.blade.php
+++ resources/views/components/campaign-card.blade.php
@@ -6,7 +6,7 @@
     'campaign_type'
 ])
 
-<div class="card shadow-sm rounded-lg overflow-hidden">
+<div class="card shadow-sm rounded-lg overflow-hidden h-100 d-flex flex-column">
     <img src="{{ asset($image) }}" alt="{{ $title }}" class="card-img-top">
     <div class="card-body">
         <h3 class="card-title ">{{ $title }}</h3>
